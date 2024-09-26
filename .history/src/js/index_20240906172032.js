f5324ec1873a85347fa7a6aee047317994a07610
robertavagyan <robertavagyan110@gmil.com>
2024-09-26 21:49:41 +0400

    first commit

---
+++ .history/src/js/index_20240906172032.js
@@ -0,0 +1,6 @@
+// import AirDatepicker from "air-datepicker";
+// import "air-datepicker/air-datepicker.css"
+// new AirDatepicker('.element')
+import Fullburger from 'fullburger';
+import 'fullburger/style';
+const fullBurger = new Fullburger('header');
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// import AirDatepicker from "air-datepicker";
+// import "air-datepicker/air-datepicker.css"
+// new AirDatepicker('.element')
+import Fullburger from 'fullburger';
+const fullBurger = new Fullburger('header');
